f4bdc210222535933602c2930dc498a07cdbfd9b
Kummirai <ajaxmilton@hotmail.com>
2024-06-17 20:57:29 +0200

    Add searchfunction

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+let search = document.querySelector('input');
+let searchBtn = document.querySelector('.search');
+
+searchBtn.addEventListener('click', searchProducts);
+
+function searchProducts() {
+  product = search.value
+  console.log(product)
+  search.value ='';
+}
