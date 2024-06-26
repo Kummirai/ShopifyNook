3253da5512af715f0ad4d333b181c7c9eb03ad28
Kummirai <ajaxmilton@hotmail.com>
2024-06-26 10:10:36 +0200

    Add products to project

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,51 @@
 const products = [
   {
-    image: "images/products/Camera Lens.jpg",
+    image: "/images/products/beadsNecklace1.jpg",
     name: "Canon Camera Lens",
     description: `Capture an astoundingly wide field - of - view <br>Perspective and create a sense of depth.<br>Keeping more of the scene in sharp focus. <br>Deep focus`,
     priceInCents: 13400
   }
   ,
   {
-    image: "images/products/Headphones.jpg",
+    image: "/images/products/beadsNecklace2.jpg",
     name: "Oneodio Pro 10",
     description: `50 mm Dual dynamic drivers <br>Ergonomic design<br>Adaptor<br>Extra bass sound`,
     priceInCents: 7550
   },
   {
-    image: "images/products/Mobile Phone.jpg",
+    image: "/images/products/beadsNecklace3.png",
     name: "Iphone 8 Plus",
     description: `Retina HD display<br>5.5 - inch(diagonal) widescreen LCD<br>Multi - Touch display with IPS technology <br>1920 - by - 1080 - pixel resolution at 401 ppi`,
     priceInCents: 39900
   },
   {
-    image: "images/products/Camera Lens.jpg",
+    image: "/images/products/bracelet4.jpg",
     name: "Canon Camera Lens",
     description: `Capture an astoundingly wide field - of - view <br>Perspective and create a sense of depth.<br>Keeping more of the scene in sharp focus. <br>Deep focus`,
     priceInCents: 13400
   }
   ,
   {
-    image: "images/products/Headphones.jpg",
+    image: "/images/products/bracelet2.jpg",
     name: "Oneodio Pro 10",
     description: `50 mm Dual dynamic drivers <br>Ergonomic design<br>Adaptor<br>Extra bass sound`,
     priceInCents: 7550
   },
   {
-    image: "images/products/Mobile Phone.jpg",
+    image: "/images/products/bracelet3.jpg",
     name: "Iphone 8 Plus",
     description: `Retina HD display<br>5.5 - inch(diagonal) widescreen LCD<br>Multi - Touch display with IPS technology <br>1920 - by - 1080 - pixel resolution at 401 ppi`,
     priceInCents: 39900
   },
   {
-    image: "images/products/Camera Lens.jpg",
+    image: "/images/products/loveNecklace1.jpg",
     name: "Canon Camera Lens",
     description: `Capture an astoundingly wide field - of - view <br>Perspective and create a sense of depth.<br>Keeping more of the scene in sharp focus. <br>Deep focus`,
     priceInCents: 13400
   }
   ,
   {
-    image: "images/products/Headphones.jpg",
+    image: "/images/products/beadsNecklace3.png",
     name: "Oneodio Pro 10",
     description: `50 mm Dual dynamic drivers <br>Ergonomic design<br>Adaptor<br>Extra bass sound`,
     priceInCents: 7550
